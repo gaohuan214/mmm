3e31db66298ded3d318c022e7ce1f6a205479f8e
gaohuan214 <18855972539@163.com>
2018-11-23 08:39:39 +0800

    mmm

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+$(function(){
+    //1.获取导航栏数据
+    $.ajax({
+        type: 'get',
+        url:'http://127.0.0.1:9090/api/getindexmenu',
+        dataType: 'json',
+        success: function(info){
+            console.log(info);
+            var htmlStr = template('navTmp',info);
+            $('.mm_nav ul').html(htmlStr);
+
+        }
+    })
+    //点击更多(事件委托),隐藏的nav li显示
+                                // 属性选择器
+      $('.mm_nav').on('click','img[alt="更多"]',function(e){
+        e.preventDefault();
+        // 用切换类的方法
+        $('.mm_nav li:nth-child(n+9)').toggleClass('hide');
+   });
+
+
+    //2.获取折扣界面数据
+    $.ajax({
+        type: 'get',
+        url: 'http://127.0.0.1:9090/api/getmoneyctrl',
+        dataType: 'json',
+        success: function(info){
+            console.log( info );
+            var htmlStr = template('infoTmp',info);
+            $('.mm_content ,info').html( htmlStr )      
+        }
+        
+    })
+
+
+    
+});
